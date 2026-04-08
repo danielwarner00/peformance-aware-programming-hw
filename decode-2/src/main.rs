3c28b9316f1fcb3868e11ca09d988f571a0d31f0
Daniel Warner <danielwarner00@gmail.com>
2026-04-08 00:29:54 -0700

    assignment 2 challenge solution

--- decode-2/src/main.rs
+++ decode-2/src/main.rs
@@ -13,71 +13,27 @@ fn main() {
             // register/memory to/from register
             let d: bool = instruction >> 1 & 1 > 0;
             let w: bool = instruction & 1 == 1;
-            let rm: u8 = instructions[1] & 0b111;
             let reg: u8 = instructions[1] >> 3 & 0b111;
-            let r#mod: u8 = instructions[1] >> 6 & 0b11;
 
             let reg_str = register_name(reg, w);
-
-            let (base_rm_str, displacement, memory, displacement_bytes) = if r#mod == 0b11 {
-                (register_name(rm, w), 0, false, 0)
-            } else if r#mod == 0b00 && rm == 0b110 {
-                (
-                    "",
-                    (instructions[2] as u16) | (instructions[3] as u16) << 8,
-                    true,
-                    2,
-                )
-            } else {
-                let (displacement, displacement_bytes) = match r#mod {
-                    0b00 => (0, 0),
-                    0b01 => (instructions[2] as u16, 1),
-                    0b10 => ((instructions[2] as u16) | (instructions[3] as u16) << 8, 2),
-                    _ => unreachable!(),
-                };
-                (
-                    [
-                        "bx + si", "bx + di", "bp + si", "bp + di", "si", "di", "bp", "bx",
-                    ][usize::from(rm)],
-                    displacement,
-                    true,
-                    displacement_bytes,
-                )
-            };
-
-            let print_rm = || {
-                if memory {
-                    if displacement > 0 {
-                        if base_rm_str.is_empty() {
-                            print!("[{displacement}]")
-                        } else {
-                            print!("[{base_rm_str} + {displacement}]")
-                        }
-                    } else {
-                        print!("[{base_rm_str}]")
-                    }
-                } else {
-                    print!("{base_rm_str}");
-                }
-            };
 
             print!("mov ");
             if d {
                 print!("{reg_str}, ");
-                print_rm();
+                print_register_memory(instructions);
                 println!();
             } else {
-                print_rm();
+                print_register_memory(instructions);
                 println!(", {reg_str}");
             };
 
-            2 + displacement_bytes
+            2 + instruction_displacement_bytes(instructions)
         } else if instruction >> 4 == 0b1011 {
             // immediate to register
             let w = instruction >> 3 & 1 == 1;
             let reg = instruction & 7;
             let reg_name = register_name(reg, w);
-            let (data, displacement_bytes) = if w {
+            let (data, data_bytes) = if w {
                 ((instructions[1] as u16) | (instructions[2] as u16) << 8, 2)
             } else {
                 (instructions[1] as u16, 1)
@@ -85,7 +41,55 @@ fn main() {
 
             println!("mov {reg_name}, {data}");
 
-            1 + displacement_bytes
+            1 + data_bytes
+        } else if instruction >> 1 == 0b1100011 {
+            // immediate to register/memory
+            assert_eq!(instructions[1] >> 3 & 7, 0);
+            let w = instruction & 1 == 1;
+            let r#mod = instructions[1] >> 6 & 3;
+
+            let data_start_index = 2 + instruction_displacement_bytes(instructions);
+
+            let (data, data_bytes) = if w {
+                (
+                    (instructions[data_start_index] as u16)
+                        | (instructions[data_start_index + 1] as u16) << 8,
+                    2,
+                )
+            } else {
+                (instructions[data_start_index] as u16, 1)
+            };
+
+            let memory = r#mod != 0b11;
+
+            print!("mov ");
+            print_register_memory(instructions);
+            let size = if memory {
+                if w { "word " } else { "byte " }
+            } else {
+                ""
+            };
+            println!(", {size}{data}");
+
+            data_start_index + data_bytes
+        } else if instruction >> 2 == 0b101000 {
+            let w = instruction & 1 == 1;
+            let d = instruction >> 1 & 1 == 1;
+
+            let address = if w {
+                instructions[1] as u16 | (instructions[2] as u16) << 8
+            } else {
+                instructions[1] as u16
+            };
+
+            print!("mov ");
+            if d {
+                println!("[{address}], ax");
+            } else {
+                println!("ax, [{address}]");
+            }
+
+            if w { 3 } else { 2 }
         } else {
             panic!();
         };
@@ -100,4 +104,69 @@ fn register_name(reg: u8, w: bool) -> &'static str {
         ["ax", "cx", "dx", "bx", "sp", "bp", "si", "di"],
     ];
     REGISTER_NAMES[w as usize][reg as usize]
+}
+
+fn make_displacement(bytes: &[u8]) -> i16 {
+    match bytes {
+        [first] => *first as i8 as i16,
+        [first, second] => (*first as u16 | (*second as u16) << 8) as i16,
+        _ => panic!(),
+    }
+}
+
+fn print_register_memory(instructions: &[u8]) {
+    let w: bool = instructions[0] & 1 == 1;
+    let rm: u8 = instructions[1] & 0b111;
+    let r#mod: u8 = instructions[1] >> 6 & 0b11;
+
+    let (base_rm_str, displacement, memory) = if r#mod == 0b11 {
+        (register_name(rm, w), 0, false)
+    } else if r#mod == 0b00 && rm == 0b110 {
+        ("", make_displacement(&instructions[2..4]), true)
+    } else {
+        let displacement = match r#mod {
+            0b00 => 0,
+            0b01 => make_displacement(&instructions[2..3]),
+            0b10 => make_displacement(&instructions[2..4]),
+            _ => unreachable!(),
+        };
+        (
+            [
+                "bx + si", "bx + di", "bp + si", "bp + di", "si", "di", "bp", "bx",
+            ][usize::from(rm)],
+            displacement,
+            true,
+        )
+    };
+
+    if memory {
+        if base_rm_str.is_empty() {
+            print!("[{displacement}]")
+        } else if displacement == 0 {
+            print!("[{base_rm_str}]")
+        } else {
+            print!("[{base_rm_str} {displacement:+}]")
+        }
+    } else {
+        print!("{base_rm_str}");
+    }
+}
+
+fn instruction_displacement_bytes(instructions: &[u8]) -> usize {
+    let rm: u8 = instructions[1] & 0b111;
+    let r#mod: u8 = instructions[1] >> 6 & 0b11;
+
+    match r#mod {
+        0b00 => {
+            if rm == 0b110 {
+                2
+            } else {
+                0
+            }
+        }
+        0b01 => 1,
+        0b10 => 2,
+        0b11 => 0,
+        _ => unreachable!(),
+    }
 }
