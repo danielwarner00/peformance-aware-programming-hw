12c0ec463450a709512b21c724d9e10f13ecd27a
Daniel Warner <danielwarner00@gmail.com>
2026-04-15 22:47:37 -0700

    output haversine sum and average

--- haversine/src/bin/generate.rs
+++ haversine/src/bin/generate.rs
@@ -14,6 +14,7 @@ fn main() {
 
     print!("{{\"pairs\":[");
     let mut first = true;
+    let mut haversine_sum = 0.0;
     for _ in 0..args.count {
         fn generate_lat() -> f64 {
             (rand::random::<f64>() * 2.0 - 1.0).asin().to_degrees() // the argument to asin will never be NaN
@@ -26,6 +27,8 @@ fn main() {
         let lat1 = generate_lat();
         let long1 = generate_long();
 
+        haversine_sum += haversine_of_degrees(lat0, long0, lat1, long1, 6371.0);
+
         if !first {
             print!(",");
         }
@@ -33,7 +36,18 @@ fn main() {
 
         first = false;
     }
-    print!("]}}");
+    let average = {haversine_sum / args.count as f64 };
+    // also output the sum and average for verifying computations
+    print!(r#"],"sum":{haversine_sum},"average":{average}}}"#);
+}
 
-    // TODO compute correct value and output
+// baseline naïve version given in the course
+fn haversine_of_degrees(lat0: f64, long0: f64, lat1: f64, long1: f64, radius: f64) -> f64 {
+    let d_y = (lat1 - lat0).to_radians();
+    let d_x = (long1 - long0).to_radians();
+    let y0 = lat0.to_radians();
+    let y1 = lat1.to_radians();
+
+    let root_term = (d_y / 2.0).sin().powi(2) + y0.cos() * y1.cos() * (d_x / 2.0).sin().powi(2);
+    2.0 * radius * root_term.sqrt().asin()
 }
